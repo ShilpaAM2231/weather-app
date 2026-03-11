6025c424022d1ae7a1ea68f79b761f030f8e6f77
ShilpaAM2231 <shilpamalladi22@gmail.com>
2026-03-12 00:03:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getWeather(){
+
+const city = document.getElementById("cityInput").value.trim()
+
+const result = document.getElementById("weatherResult")
+
+if(city === ""){
+result.innerHTML = "⚠ Please enter a city name"
+return
+}
+
+const apiKey = "5fa48fefcbe40e2cdc7818656227d847"
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+const response = await fetch(url)
+
+const data = await response.json()
+
+if(data.cod != 200){
+result.innerHTML = "❌ City not found. Try another city."
+return
+}
+
+result.innerHTML = `
+<h3>${data.name}</h3>
+<p>Temperature: ${data.main.temp} °C</p>
+<p>Weather: ${data.weather[0].description}</p>
+<p>Humidity: ${data.main.humidity}%</p>
+<p>Wind Speed: ${data.wind.speed} km/h</p>
+`
+
+}
