ee3a0acec412baa1045d93771727a8e312198b96
ShilpaAM2231 <shilpamalladi22@gmail.com>
2026-03-12 00:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ result.innerHTML = "⚠ Please enter a city name"
 return
 }
 
-const apiKey = "XXXXXXXXX"
+const apiKey = "YOUR_API_KEY"
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
@@ -31,3 +31,4 @@ result.innerHTML = `
 `
 
 }
+
